02812d40c1ac519016a054c078fb0157320341f0
unknown <you@example.com>
2023-03-24 12:51:24 +0530

    Assignment

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -7,7 +7,9 @@ public class BankAccount {
     private double minBalance;
 
     public BankAccount(String name, double balance, double minBalance) {
-
+        this.name = name;
+        this.balance = balance;
+        this.minBalance = minBalance;
     }
 
     public String generateAccountNumber(int digits, int sum) throws Exception{
@@ -15,17 +17,58 @@ public class BankAccount {
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
 
-        return null;
+        if(sum > 9*digits){
+           throw  new Exception("Account Number can not be generated");
+        }
+        String accNo = "";
+        while(sum > 9){
+            accNo += "9";
+            sum -= 9;
+        }
+        // adding remainder
+        accNo += sum + "";
+        while(accNo.length() < digits){
+            accNo += "0";
+        }
+        return accNo;
     }
 
     public void deposit(double amount) {
         //add amount to balance
-
+        this.balance += amount;
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-
+        if(amount > this.balance){
+            throw  new Exception("Insufficient Balance");
+        }
+        else if(balance - amount >= minBalance){
+            balance -= amount;
+        }
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
+
+    public void setMinBalance(double minBalance) {
+        this.minBalance = minBalance;
+    }
 }
--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -5,7 +5,8 @@ public class CurrentAccount extends BankAccount{
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
-
+        super(name,balance,5000);
+        this.tradeLicenseId = tradeLicenseId;
     }
 
     public void validateLicenseId() throws Exception {
@@ -13,7 +14,54 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
+        if(!isValid(tradeLicenseId)){
+            String validId = rearrange(tradeLicenseId);
+            if(validId.equals("")){
+                throw  new Exception("Valid License can not be generated");
+            }
+            else this.tradeLicenseId = validId;
+        }
 
     }
-
+    public boolean isValid(String s){
+        for(int i=0;i<s.length()-1;i++){
+            if(s.charAt(i) == s.charAt(i+1)) return false;
+        }
+        return true;
+    }
+    public String rearrange(String s){
+        int[] freq = new int[26];
+        // storing the freq of characters in String
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i) - 'a']++;
+        }
+        // maxFreq char
+        int max = 0,c = 0;
+        for(int i=0;i<26;i++){
+            if(max < freq[i]){
+                max = freq[i];
+                c = i;
+            }
+        }
+        if(max > (s.length()+1)/2) return "";
+        char[] ch = new char[s.length()];
+        int idx = 0;
+        while(freq[c] > 0){
+            char letter = (char) (c+'a');
+            ch[idx] = letter;
+            idx += 2;
+            freq[c]--;
+        }
+        for(int i=0;i<s.length();i++){
+            while(freq[i] > 0){
+                if(idx >= s.length()){
+                    idx = 1;
+                }
+                ch[idx] = (char)(i+'a');
+                idx += 2;
+                freq[i]--;
+            }
+        }
+        return String.valueOf(ch);
+    }
 }
